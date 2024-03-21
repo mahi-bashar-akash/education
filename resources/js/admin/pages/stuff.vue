7c412c8f2a7cf872f4f84a8d61a1b3e4b52a492d
mahibashardeveloper <mahibashar2023@gmail.com>
2024-03-21 22:18:39 +0600

    update changes

--- resources/js/admin/pages/stuff.vue
+++ resources/js/admin/pages/stuff.vue
@@ -1,6 +1,31 @@
 <template>
 
-
+    <!-- breadcrumb -->
+    <div class="d-sm-flex justify-content-between align-items-center">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <router-link :to="{name: 'stuff'}" class="text-decoration-none text-light-gray">
+                        Stuff
+                    </router-link>
+                </li>
+            </ol>
+        </nav>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <router-link :to="{name: 'dashboard'}" class="text-decoration-none text-light-gray">
+                        Dashboard
+                    </router-link>
+                </li>
+                <li class="breadcrumb-item">
+                    <router-link :to="{name: 'stuff'}" class="text-decoration-none text-light-gray">
+                        Stuff
+                    </router-link>
+                </li>
+            </ol>
+        </nav>
+    </div>
 
 </template>
 
