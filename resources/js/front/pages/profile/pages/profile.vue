3c186d31d1523b3dc3e710267651bf4615848e07
mahibashardeveloper <mahibashar2023@gmail.com>
2024-03-28 09:57:25 +0600

    update profile

--- resources/js/front/pages/profile/pages/profile.vue
+++ resources/js/front/pages/profile/pages/profile.vue
@@ -27,15 +27,33 @@
         <div class="mb-2 fs-5">Edit password</div>
         <div class="mb-2">
             <label for="current-password" class="form-label">Current password</label>
-            <input id="current-password" type="password" name="current-password" class="form-control" required autocomplete="current-password">
+            <div class="position-relative">
+                <input id="current-password" :type="currentPasswordFieldType" name="password" class="form-control" required autocomplete="new-current-password">
+                <div class="me-3 border-0 bg-transparent position-absolute top-50 end-0 translate-middle-y me-2 cursor-pointer" @click="currentPasswordVisibility">
+                    <i class="bi bi-eye" v-if="currentPasswordFieldType === 'text'"></i>
+                    <i class="bi bi-eye-slash" v-if="currentPasswordFieldType === 'password'"></i>
+                </div>
+            </div>
         </div>
-        <div class="mb-2">
-            <label for="new-password" class="form-label">New password</label>
-            <input id="new-password" type="password" name="new-password" class="form-control" required autocomplete="new-password">
+        <div class="form-group mb-3">
+            <label for="password" class="form-label">Password</label>
+            <div class="position-relative">
+                <input id="password" :type="passwordFieldType" name="password" class="form-control" required autocomplete="new-password">
+                <div class="me-3 border-0 bg-transparent position-absolute top-50 end-0 translate-middle-y me-2 cursor-pointer" @click="passwordVisibility">
+                    <i class="bi bi-eye" v-if="passwordFieldType === 'text'"></i>
+                    <i class="bi bi-eye-slash" v-if="passwordFieldType === 'password'"></i>
+                </div>
+            </div>
         </div>
-        <div class="mb-2">
-            <label for="confirm-password" class="form-label">Confirm password</label>
-            <input id="confirm-password" type="password" name="confirm-password" class="form-control" required autocomplete="confirm-password">
+        <div class="form-group mb-3">
+            <label for="confirm-password" class="form-label">Confirm Password</label>
+            <div class="position-relative">
+                <input id="confirm-password" :type="passwordConfirmationFieldType" name="confirm-password" class="form-control" required autocomplete="new-confirm-password">
+                <div class="me-3 border-0 bg-transparent position-absolute top-50 end-0 translate-middle-y me-2 cursor-pointer" @click="passwordConfirmVisibility">
+                    <i class="bi bi-eye" v-if="passwordConfirmationFieldType === 'text'"></i>
+                    <i class="bi bi-eye-slash" v-if="passwordConfirmationFieldType === 'password'"></i>
+                </div>
+            </div>
         </div>
         <button type="submit" class="btn btn-theme rounded-0">
             Update password
@@ -43,22 +61,26 @@
     </form>
 
     <form class="mb-3 bg-white shadow p-3">
-        <div class="mb-2 fs-5">Edit payment</div>
+        <div class="mb-2 fs-5">Add payment</div>
         <div class="mb-2">
-            <label for="paymentMethod" class="form-label d-block">Payment methods</label>
-            <div class="mb-2">
-                <label for="cash-in-hand" class="form-check-label text-light-gray-hover d-flex align-items-center">
-                    <input id="cash-in-hand" type="radio" class="form-radio" name="payment-method">
-                    <span class="ms-2"> Cash in hand </span>
-                </label>
-            </div>
-            <div class="mb-2">
-                <label for="banking" class="form-check-label text-light-gray-hover d-flex align-items-center">
-                    <input id="banking" type="radio" class="form-radio" name="payment-method">
-                    <span class="ms-2"> Banking </span>
-                </label>
-            </div>
+            <label for="card-holder-name" class="form-label">Card holder name</label>
+            <input id="card-holder-name" type="text" name="card-holder-name" class="form-control" required autocomplete="new-card-holder-name">
         </div>
+        <div class="mb-2">
+            <label for="card-name" class="form-label">Card name</label>
+            <input id="card-name" type="text" name="card-name" class="form-control" required autocomplete="new-card-name">
+        </div>
+        <div class="mb-2">
+            <label for="expire-date" class="form-label">Expire date</label>
+            <input id="expire-date" type="text" name="expire-date" class="form-control" required autocomplete="new-expire-date">
+        </div>
+        <div class="mb-2">
+            <label for="cvv" class="form-label">CVV</label>
+            <input id="cvv" type="text" name="cvv" class="form-control" required autocomplete="new-cvv">
+        </div>
+        <button type="submit" class="btn btn-theme rounded-0">
+            Submit
+        </button>
     </form>
 
 </template>
@@ -73,11 +95,32 @@ export default {
                 email: 'mahibashar2023@gmail.com',
                 phoneNumber: '01400125289',
                 address: 'Dhanmondi, Dhaka - 1209, Bangladesh',
-            }
+            },
+            password: '',
+            currentPasswordFieldType: 'password',
+            passwordFieldType: 'password',
+            passwordConfirmationFieldType: 'password',
         }
     },
     mounted() {  },
-    methods: {  }
+    methods: {
+
+        /* Function to current password visibility */
+        currentPasswordVisibility() {
+            this.currentPasswordFieldType = this.currentPasswordFieldType === "password" ? "text" : "password";
+        },
+
+        /* Function to password visibility */
+        passwordVisibility() {
+            this.passwordFieldType = this.passwordFieldType === "password" ? "text" : "password";
+        },
+
+        /* Function to password confirm visibility */
+        passwordConfirmVisibility() {
+            this.passwordConfirmationFieldType = this.passwordConfirmationFieldType === "password" ? "text" : "password";
+        },
+
+    }
 }
 
 </script>
