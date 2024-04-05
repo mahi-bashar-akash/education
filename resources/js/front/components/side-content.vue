e72632875360df8b26e2d3825830e5ea451319cf
mahibashardeveloper <mahibashar2023@gmail.com>
2024-04-05 15:22:19 +0600

    update changes

---
+++ resources/js/front/components/side-content.vue
@@ -0,0 +1,133 @@
+<template>
+
+    <!-- search -->
+    <div class="position-relative">
+        <input type="text" name="keyword" class="form-control ps-5" required autocomplete="new-search"
+               placeholder="Write your keyword..">
+        <div class="position-absolute top-50 start-0 translate-middle-y ps-3">
+            <i class="bi bi-search"></i>
+        </div>
+    </div>
+
+    <!-- recent posts -->
+    <div class="my-4">
+
+        <div class="fw-bold fs-5 mb-3">Recent Posts</div>
+
+        <div class="mb-3">
+            <a href="javascript:void(0)"
+               class="text-decoration-none text-light-gray-hover d-flex align-items-start justify-content-start">
+                <span>
+                    <img :src="`/images/blogs/blog-1.jpg`" class="wpx-120 hpx-84 rounded-3 img-fluid object-fit-cover" alt="course">
+                </span>
+                <span class="ms-2">
+                    <span class="d-block">
+                        Cupidatat non proident sunt culpa officia deserunt
+                    </span>
+                    <span class="d-block text-warning">
+                        Price $45
+                    </span>
+                </span>
+            </a>
+        </div>
+
+        <div class="mb-3">
+            <a href="javascript:void(0)" class="text-decoration-none text-light-gray-hover d-flex align-items-start justify-content-start">
+                <span>
+                    <img :src="`/images/blogs/blog-2.jpg`" class="wpx-120 hpx-84 rounded-3 img-fluid object-fit-cover" alt="course">
+                </span>
+                <span class="ms-2">
+                    <span class="d-block">
+                        Cupidatat non proident sunt culpa officia deserunt
+                    </span>
+                    <span class="d-block text-warning">
+                        Price $45
+                    </span>
+                </span>
+            </a>
+        </div>
+
+        <div class="mb-3">
+            <a href="javascript:void(0)" class="text-decoration-none text-light-gray-hover d-flex align-items-start justify-content-start">
+                <span>
+                    <img :src="`/images/blogs/blog-3.jpg`"
+                         class="wpx-120 hpx-84 rounded-3 img-fluid object-fit-cover" alt="course">
+                </span>
+                <span class="ms-2">
+                    <span class="d-block">
+                        Cupidatat non proident sunt culpa officia deserunt
+                    </span>
+                    <span class="d-block text-warning">
+                        Price $45
+                    </span>
+                </span>
+            </a>
+        </div>
+
+    </div>
+
+    <!-- categories -->
+    <div class="my-4">
+
+        <div class="fw-bold fs-5 mb-3">Categories</div>
+
+        <a href="javascript:void(0)" class="text-decoration-none text-light-gray-hover d-flex align-items-center justify-content-start py-1 mb-2">
+            <i class="bi bi-chevron-right me-2"></i>
+            Home teaching (1)
+        </a>
+
+        <a href="javascript:void(0)" class="text-decoration-none text-light-gray-hover d-flex align-items-center justify-content-start py-1 mb-2">
+            <i class="bi bi-chevron-right me-2"></i>
+            Education (1)
+        </a>
+
+        <a href="javascript:void(0)" class="text-decoration-none text-light-gray-hover d-flex align-items-center justify-content-start py-1 mb-2">
+            <i class="bi bi-chevron-right me-2"></i>
+            Learning (2)
+        </a>
+
+        <a href="javascript:void(0)" class="text-decoration-none text-light-gray-hover d-flex align-items-center justify-content-start py-1 mb-2">
+            <i class="bi bi-chevron-right me-2"></i>
+            Studentship (3)
+        </a>
+
+    </div>
+
+    <!-- blog sidebar img -->
+    <div class="my-4">
+        <div class="card text-bg-dark border-0 rounded-0">
+            <img :src="`/images/sidebar-img.jpg`" class="card-img hpx-350 object-fit-fill" alt="sidebar-img">
+            <div class="card-img-overlay d-flex justify-content-start align-items-start flex-column">
+                <div class="mb-4">
+                    <div class="p-3 rounded-circle wpx-60 hpx-60 d-flex justify-content-center align-items-center bg-theme text-white fw-medium">
+                        Free
+                    </div>
+                </div>
+                <div class="p-3 fw-bold text-white fs-4 mb-4">
+                    Fill Your Sketchbook & Build a Watercolor Habit
+                </div>
+                <button type="button" class="btn btn-warning w-100 rounded-pill fw-medium">
+                    Get Started Today
+                </button>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+
+    data() {
+        return {}
+    },
+
+    mounted() {
+    },
+
+    methods: {}
+
+}
+
+</script>
--- resources/js/front/pages/blog/blog.vue
+++ resources/js/front/pages/blog/blog.vue
@@ -2,55 +2,66 @@
 
     <div class="cursor-content margin-top-105">
         <div class="container">
-            <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
+            <div class="row">
+                <div class="col-lg-9 py-4">
+                    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
 
-                <div class="p-3" v-for="each in blogDataList">
-                    <div class="card shadow rounded-3 overflow-hidden bg-light border-0 p-0 image-effect">
-                        <div class="card-body p-0 rounded-3">
-                            <div class="overflow-hidden hpx-250">
-                                <img :src="each.filePath" class="img-fluid object-fit-cover w-100" alt="brand 2">
-                            </div>
-                            <div class="pt-3 px-3">
-                                <div class="w-100 d-flex justify-content-between align-items-center">
-                                    <div class="badge bg-warning text-dark fw-medium px-3 py-2 rounded-pill">
-                                        {{ each.author }}
+                        <div class="p-3" v-for="each in blogDataList">
+                            <div class="card shadow rounded-3 overflow-hidden bg-light border-0 p-0 image-effect">
+                                <div class="card-body p-0 rounded-3">
+                                    <div class="overflow-hidden hpx-200">
+                                        <img :src="each.filePath" class="img-fluid object-fit-cover w-100" alt="brand 2">
                                     </div>
-                                    <div class="text-secondary text-opacity-75">
-                                        {{ each.date }}
+                                    <div class="pt-3 px-3">
+                                        <div class="w-100 d-flex justify-content-between align-items-center">
+                                            <div class="badge bg-warning text-dark fw-medium px-3 py-2 rounded-pill">
+                                                {{ each.author }}
+                                            </div>
+                                            <div class="text-secondary text-opacity-75">
+                                                {{ each.date }}
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="fs-3 fw-medium truncate-to-2-line p-3 text-light-gray-hover">
-                                {{ each.title }}
-                            </div>
-                            <div class="pb-3 px-3">
-                                <router-link :to="{name: 'singleBlog'}" class="text-decoration-none">
+                                    <div class="fs-3 fw-medium truncate-to-2-line p-3 text-light-gray-hover">
+                                        {{ each.title }}
+                                    </div>
+                                    <div class="pb-3 px-3">
+                                        <router-link :to="{name: 'singleBlog'}" class="text-decoration-none">
                                     <span class="text-secondary">
                                         Read more
                                     </span>
-                                    <i class="bi bi-arrow-right ms-2 text-dark"></i>
-                                </router-link>
+                                            <i class="bi bi-arrow-right ms-2 text-dark"></i>
+                                        </router-link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
 
+                    </div>
+
+                    <!-- pagination -->
+                    <pagination/>
+
+                </div>
+                <div class="col-lg-3 py-4">
+
+                    <sideContent/>
+
+                </div>
             </div>
         </div>
-
-        <!-- pagination -->
-        <pagination/>
 
     </div>
 
 </template>
 
 <script>
+import sideContent from "../../components/side-content.vue";
 import pagination from "../../components/pagination.vue";
 
 export default {
     components: {
-        pagination
+        sideContent, pagination
     },
     data() {
         return {
